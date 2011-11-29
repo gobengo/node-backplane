a158be90682739cd2c0950e887923cd2351fa539
Benjamin Goering <bengoering@gmail.com>
2011-11-29 08:55:12 -0800

    Too many changes to note. Sorry, it was in a weird state for awhile

--- server.js
+++ server.js
@@ -1,8 +1,27 @@
 var Backplane = require('Backplane')
+	,	connect = require('connect')
+
+var server = connect(
+				connect.logger()
+			,	connect.methodOverride()
+		)
+	,	BP = new Backplane(server)
+
+setInterval(function() {
+  function randomString(len, charSet) {
+    charSet = charSet || 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+    var randomString = '';
+    for (var i = 0; i < len; i++) {
+        var randomPoz = Math.floor(Math.random() * charSet.length);
+        randomString += charSet.substring(randomPoz,randomPoz+1);
+    }
+    return randomString;
+  }
+  BP.emit('message', randomString(26))
+}, 1000)
 
 var PORT = 3000
-
-new Backplane().listen(PORT, function() {
+BP.server.listen(PORT, function() {
 	console.info('Listening on port '+PORT+'…')
 })
 
